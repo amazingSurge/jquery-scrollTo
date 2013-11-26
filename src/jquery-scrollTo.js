f898590001ed56c7022775911ed00c03e8b5543a
iejerry <546842522@qq.com>
2013-11-26 10:26:06 +0800

    update

--- src/jquery-scrollTo.js
+++ src/jquery-scrollTo.js
@@ -20,11 +20,11 @@
 		this.activeClass = this.namespace + '_active';
 
 		this.noroll = false;
-		this.sheet = document.styleSheets[document.styleSheets.length - 1];
 
 		var self = this;
 		$.extend(self, {
 			init: function() {
+				self.transition = self.transition();
 				self.build();
 				self.roll();
 
@@ -33,6 +33,8 @@
 					var target = event.target || event.srcElement;
 					self.$target = $(target);
 					self.active(self.$target);
+					var href = self.$target.attr('href');
+					self.$anchor = $(href);
 					self.$doc.trigger('ScrollTo::jump');
 					return false;
 				});
@@ -40,37 +42,36 @@
 				//bind events
 				self.$doc.on('ScrollTo::jump', function() {
 					self.noroll = true;
-					var href = self.$target.attr('href');
-					var $actualAnchor = $(href);
 
-					if ($actualAnchor && $actualAnchor.length > 0) {
-						var top = $actualAnchor.offset().top;
-						if (self.sheet.addRule) {
+					if (self.$anchor && self.$anchor.length > 0) {
+						var top = self.$anchor.offset().top;
+						if (self.transition.supported) {
+							var pos = $(window).scrollTop();
+							self.$doc.css({
+								'margin-top': -(pos - top) + 'px'
+							});
+							$(window).scrollTop(top);
+							self.$doc.addClass(self.easing).css({
+								'margin-top': ''
+							});
+						} else {
 							$('body, html').stop(true, false).animate({
 								scrollTop: top
 							}, self.options.speed);
 							return;
 						}
-						var pos = $(window).scrollTop();
-						self.$doc.css({
-							'margin-top': -(pos - top) + 'px'
-						});
-						$(window).scrollTop(top);
-						self.$doc.addClass(self.easing).css({
-							'margin-top': ''
-						});
 					} else {
 						return;
 					}
 				});
-				self.$doc.on('webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd', function() {
+				self.$doc.on(self.transition.end, function() {
 					self.noroll = false;
 					self.$doc.removeClass(self.easing);
 				});
 
 			},
 			build: function() {
-				self.insertRule(self.sheet, '.' + self.easing, '-webkit-transition-duration: ' + self.options.speed + 'ms; transition-duration: ' + self.options.speed + 'ms;', 0);
+				self.insertRule('.' + self.easing + '{' + self.transition.prefix + 'transition-duration: ' + self.options.speed + 'ms;}');
 			},
 			active: function($index) {
 				if ($index.parent().parent().has('.' + self.activeClass).length) {
@@ -79,28 +80,64 @@
 				} else {
 					$index.addClass(self.activeClass);
 				}
+				var href = $index.attr('href');
+				self.$anchor = $(href);
 			},
 			roll: function() {
 				if (self.noroll) {
 					return;
 				}
 				self.$doc.find("[id]").each(function() {
-					if ($(window).scrollTop() > $(this).offset().top - 100 && $(window).scrollTop() < $(this).offset().top + $(this).parent().height()) {
+					if (($(window).scrollTop() > $(this).offset().top - self.options.offsetTop) && ($(window).scrollTop() < $(this).offset().top + $(this).height())) {
 						var anchor_href = $(this).attr('id');
 						var $anchor = self.$element.find('[href="#' + anchor_href + '"]');
 						self.active($anchor);
 					}
 				});
 			},
-			insertRule: function(sheet, selectorText, cssText, position) {
-				try {
-					if (sheet.insertRule) {
-						sheet.insertRule(selectorText + "{" + cssText + "}", position);
-					} else if (sheet.addRule) {
-						sheet.addRule(selectorText, cssText, position);
+			transition: function() {
+				var e,
+					end,
+					prefix = '',
+					supported = false,
+					el = document.createElement("fakeelement"),
+					transitions = {
+						"WebkitTransition": "webkitTransitionEnd",
+						"MozTransition": "transitionend",
+						"OTransition": "oTransitionend",
+						"transition": "transitionend"
+					};
+				for (e in transitions) {
+					if (el.style[e] !== undefined) {
+						end = transitions[e];
+						supported = true;
+						break;
 					}
-				} catch (e) {
-					$('head').append("<style>" + selectorText + "{" + cssText + "}" + "</style>");
+				}
+				if (/(WebKit)/i.test(window.navigator.userAgent)) {
+					prefix = '-webkit-';
+				}
+				return {
+					prefix: prefix,
+					end: end,
+					supported: supported
+				};
+			},
+			insertRule: function(rule) {
+				if (self.rules && self.rules[rule]) {
+					return;
+				} else if (self.rules === undefined) {
+					self.rules = {};
+				} else {
+					self.rules[rule] = true;
+				}
+
+				if (document.styleSheets && document.styleSheets.length) {
+					document.styleSheets[0].insertRule(rule, 0);
+				} else {
+					var style = document.createElement('style');
+					style.innerHTML = rule;
+					document.head.appendChild(style);
 				}
 			}
 		});
@@ -113,7 +150,8 @@
 	ScrollTo.defaults = {
 		speed: '1000',
 		easing: 'linear',
-		namespace: 'scrollTo'
+		namespace: 'scrollTo',
+		offsetTop: 50
 	};
 	ScrollTo.prototype = {
 		constructor: ScrollTo,
